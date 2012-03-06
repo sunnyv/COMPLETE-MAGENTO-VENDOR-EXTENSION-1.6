c766762d41a821a1f67ac336f56fe6c0797cb84e
sunny sharma <sharma.viplaish@yahoo.com>
2012-03-06 12:54:51 +0530

    custom1

--- app/code/local/Vendor/Vcatalog/Model/Observer.php
+++ app/code/local/Vendor/Vcatalog/Model/Observer.php
@@ -1,10 +1,8 @@
 <?php
 class Vendor_Vcatalog_Model_Observer extends Mage_Core_Model_Abstract
 {
-
      public function updatevEndorProfileStatus()
 	    {
-		  
 		  #get logged in vendor id 23-2-11
           $loggedInVendorId=Mage::getSingleton('admin/session')->getUser()->user_id;
           $vinfoCollection=Mage::getModel('vcatalog/vinfo')->getCollection()->addFieldToFilter('admin_user',$loggedInVendorId)->getData();
